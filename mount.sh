c8d028b65cb62fbde32ad5e4126622e3d8c3a8c3
Gleb <g.lukicov@ucl.ac.uk>
2020-07-09 17:13:24 +0100

    Added mounting script for macOS FUSE

---
+++ mount.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#-----------------------------------------------------------
+remotedir="USERNAME@HOST:/PATH/TO/MOUNT"  # TODO Add directory on the remote machine
+mountdir="/Users/user/Mount/Directory"   # TODO Add local directory 
+echo "Mounting (${mountdir})..."
+
+if [ -d "${mountdir}" ]; then
+  #Unmount if it's already mounted
+  if mount | grep "on $mountdir" > /dev/null; then
+    umount ${mountdir}  
+  fi
+else
+  mkdir ${mountdir}
+fi
+
+#Now mount the remote file system
+#   (The -o workaround=rename -o noexec are used so kwrite can save files.)
+sshfs -o ssh_command='ssh -AKXY' -o workaround=rename -o noexec -o idmap=user -o uid=$(id -u) -o gid=$(id -g) ${remotedir} ${mountdir}
+
+#Sleep 1 second to give it time to register, then check that it is now mounted
+sleep 1
+if mount | grep "on $mountdir" > /dev/null; then
+  echo "Success"
+  cd ${mountdir}
+else
+  echo "Failure!"  
+fi
+
+
+
+# exit 0
