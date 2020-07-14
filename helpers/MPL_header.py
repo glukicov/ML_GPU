4ebe7323f4a01ca8ec70540932dd81c40a6884a3
glukicov <g.lukicov@ucl.ac.uk>
2020-07-14 20:18:32 +0100

    Added MPL style file

--- helpers/MPL_header.py
+++ helpers/MPL_header.py
@@ -1,14 +1,6 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import os
-# mpl.use('Agg') # batch-mode 
-mpl.rc('axes', labelsize=14)
-mpl.rc('xtick', labelsize=12)
-mpl.rc('ytick', labelsize=12)
-# plt.style.use('fivethirtyeight')
-# plt.style.use('dark_background')
 path_to_file= os.path.join(os.environ.get('ML_PATH'),"helpers","ml_style.mplstyle")
 plt.style.use('file://'+path_to_file)
-
-# mpl.rc('color', 'green')
-# mpl.rc('grid', False)
+# mpl.use('Agg') # batch-mode 
