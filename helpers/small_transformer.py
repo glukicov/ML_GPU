ce4c238be5e08218f2e5ae2eef9bf3ba96b9ac7c
glukicov <g.lukicov@ucl.ac.uk>
2020-07-15 21:29:46 +0100

    Working on chapter 2

---
+++ helpers/small_transformer.py
@@ -0,0 +1,27 @@
+'''
+Gleb Lukicov
+small transformer class example (original code by handsonml2)
+* hyperparameter, add_bedrooms_per_room
+
+TODO: Adopt to general cases
+
+'''
+from sklearn.base import BaseEstimator, TransformerMixin
+
+# column index
+rooms_ix, bedrooms_ix, population_ix, households_ix = 3, 4, 5, 6
+
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, add_bedrooms_per_room = True): # no *args or **kargs
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+    def fit(self, X, y=None):
+        return self  # nothing else to do
+    def transform(self, X):
+        rooms_per_household = X[:, rooms_ix] / X[:, households_ix]
+        population_per_household = X[:, population_ix] / X[:, households_ix]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+            return np.c_[X, rooms_per_household, population_per_household,
+                         bedrooms_per_room]
+        else:
+            return np.c_[X, rooms_per_household, population_per_household]
