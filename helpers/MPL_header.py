036b013b0bd7d466befd383d84137bc297f4e845
glukicov <g.lukicov@ucl.ac.uk>
2020-07-15 11:06:14 +0100

    Working on chapter 2

--- helpers/MPL_header.py
+++ helpers/MPL_header.py
@@ -3,4 +3,9 @@ import matplotlib.pyplot as plt
 import os
 path_to_file= os.path.join(os.environ.get('ML_PATH'),"helpers","ml_style.mplstyle")
 plt.style.use('file://'+path_to_file)
+
+#colorbar hacks (non-pandas version)
+cbar.ax.tick_params(labelsize=8)
+cbar.ax.set_ylabel(fontsize=10)
+
 # mpl.use('Agg') # batch-mode 
