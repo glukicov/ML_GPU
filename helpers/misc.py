e499a245d9cbff8807aacae6258af7fd9d593b33
glukicov <g.lukicov@ucl.ac.uk>
2020-07-21 17:13:16 +0100

    on chapter 2

--- helpers/misc.py
+++ helpers/misc.py
@@ -3,8 +3,34 @@ Author: Gleb Lukicov
 
 Auxiliary functions 
 '''
+import numpy as np 
+from scipy import stats
+
+### Stat functions
+def ci(pred, test, cl=0.95):
+    print("Calculating a",int(cl*100),"% CI...")
+    squared_errors = (pred - test) ** 2
+    DoF = len(squared_errors) - 1
+    sample_mean = squared_errors.mean()
+    sample_standard_error = stats.sem(squared_errors)
+    return np.sqrt(  stats.t.interval( cl, DoF, loc=sample_mean, scale= sample_standard_error )  )
+
+
+### Print-out functions 
 
 def scores_stats(scroes):
+    '''
+    Given array for scores, print basic stats 
+    '''
     print('scores:',  scroes)
     print("<scores>:", scroes.mean())
     print("𝝈(scores):", scroes.std())
+
+def params_grid(cv_results):
+    '''
+    Given grid search, print all scores (sorted)
+    '''
+    # for mean_score, params in zip(cv_results["mean_test_score"], cv_results["params"]):
+    dict_results  = sorted(dict(zip(np.sqrt(-cv_results["mean_test_score"]), cv_results["params"])).items())
+    for item in dict_results:
+        print (item)
