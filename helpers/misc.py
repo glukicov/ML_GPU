f9552534086fb94337670542d1e43fdf7e8b7ea5
glukicov <g.lukicov@ucl.ac.uk>
2020-07-27 20:46:58 +0100

    Working on chapter 3

--- helpers/misc.py
+++ helpers/misc.py
@@ -1,17 +1,19 @@
-'''
+"""e
 Author: Gleb Lukicov
 
-Auxiliary functions 
-'''
+Auxiliary functions
+"""
+from sklearn.model_selection import StratifiedKFold 
+from sklearn.base import clone
 import numpy as np 
 from scipy import stats
 
 ### Stat functions
 
 def ci(pred, test, cl=0.95):
-    '''
-    t-score CI 
-    '''
+    """
+    t-score CI
+    """
     print("Calculating a",int(cl*100),"% CI...")
     squared_errors = (pred - test) ** 2
     DoF = len(squared_errors) - 1
@@ -58,3 +60,30 @@ def save_fig(fig_path=".", fig_id="0", fig_extension="png", tight_layout=True, ,
     if tight_layout:
         plt.tight_layout()
     plt.savefig(path, format=fig_extension, dpi=resolution)
+
+
+### ML functions
+
+
+def misc_cross_val_score(X_train, y_train, sgd_clf, cv=3):
+    """
+    #  Implimentation of the cross_val_score function
+    # Crdeit: https://github.com/ageron/handson-ml2 
+    The StratifiedKFold class performs stratified sampling,
+    to produce folds that contain a representative ratio of each class. 
+    At each iteration the code creates a clone of the classifier, 
+    trains that clone on the training folds, and makes predictions on the test fold. 
+    Then it counts the number of correct predictions and outputs the ratio of correct predictions.
+    """
+    skfolds = StratifiedKFold(n_splits=cv, random_state=42)
+    for train_index, test_index in skfolds.split(X_train, y_train):
+        clone_clf = clone(sgd_clf)
+        X_train_folds = X_train[train_index]
+        y_train_folds = y_train[train_index]
+        X_test_fold = X_train[test_index] y_test_fold = y_train[test_index]
+        clone_clf.fit(X_train_folds, y_train_folds)
+        y_pred = clone_clf.predict(X_test_fold)
+        n_correct = sum(y_pred == y_test_fold)
+        print(n_correct / len(y_pred))  # prints e.g. 0.9502, 0.96565, and 0.96495
+
+
