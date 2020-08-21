526a8128136684de9010a7e8dca1388ca66c2458
Gleb Lukicov <g.lukicov@ucl.ac.uk>
2020-08-21 22:20:23 +0100

    Working on chapter 4

--- helpers/header.py
+++ helpers/header.py
@@ -18,3 +18,10 @@ SUB_DIR = "classification"
 IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "fig", SUB_DIR)
 # IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "fig")
 os.makedirs(IMAGES_PATH, exist_ok=True)
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
